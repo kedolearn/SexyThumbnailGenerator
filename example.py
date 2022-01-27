3d0f68e40397eb5ccbf244726e384c836a56d4bf
thearyadev <87589047+thearyadev@users.noreply.github.com>
2022-01-27 14:42:12 -0500

    Create example.py

---
+++ example.py
@@ -0,0 +1,7 @@
+from SexyThumbnailGenerator import Generator
+
+import SexyThumbnailGenerator
+# from src.SexyThumbnailGenerator import Generator
+x = Generator("X:\Aryan\Media\Film Database\\00590\\film.mp4")
+x.generateSelection()
+x.save()
